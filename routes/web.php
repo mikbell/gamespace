f6107723d1c0e31cc53795d6bbc9dabd24731f5a
Michele Campanello <Campanellomichele@outlook.it>
2024-10-21 19:45:49 +0200

    design pagina index

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::view('/', 'index');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
